459f0306f9af521e8829ea829e4b6cf86d34d6fb
Sourav Maity <112808784+sou-rav-23@users.noreply.github.com>
2024-01-31 18:10:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-from pytube import YouTube;
+from flask import Flask, render_template, request, jsonify
+from pytube import YouTube
 
 app = Flask(__name__)
 
@@ -9,19 +9,15 @@ def index():
 
 @app.route('/download', methods=['POST'])
 def download():
-    try:
-        url = request.form['url']
-        yt = YouTube(url)
-        # video = yt.streams.filter(file_extension='mp4', progressive=True).first()
-        video=yt.streams.filter(resolution="720p",file_extension="mp4").first()
-    
-        video.download('downloads/')  # Save the video in the 'downloads' folder
-        # return render_template('index.html', message='      Downloading...🔃     ')
-        # display("      Downloading...🔃     ",target="message")
-        return render_template('index.html', message='Downloaded successful!')
-    except Exception as e:
-        return render_template('index.html', message=f'Download failed: {str(e)}')
+    video_link = request.form['video_link']
 
+    try:
+        youtube = YouTube(video_link)
+        video_stream = youtube.streams.filter(file_extension="mp4").first()
+        video_stream.download('./downloads')  # You can customize the download directory
+        return jsonify({'success': True, 'message': 'Video downloaded successfully.'})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
